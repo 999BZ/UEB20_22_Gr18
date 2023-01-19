53d717edceb9f0018090f35538348fa6ed97915d
999BZ <blendizeqiri@hotmail.com>
2023-01-19 18:14:35 +0100

    Update

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,20 @@ let date = today.toISOString().slice(0,10);
 var pdata = document.getElementById("datasot");
 
 pdata.innerHTML = date;
+
+
+$("#safePar").hide();
+$("#safeFLIX").click(function () {
+    
+    if($("#safePar").is(':visible')){
+        $("#safeFLIX").css("color", "black");
+        $("#safePar").fadeOut(2000).slideUp(3000);
+    }else{
+        $("#safeFLIX").css("color", "red");
+        $("#safePar").show();
+    }
+});
+
+$("#mydiv").animate({
+    width: '200px'
+}, 3000);
